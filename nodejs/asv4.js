999d0964a047629224178b94efd31cf999ad6a31
Didier Parat <didier.parat@telenordigital.com>
2018-09-02 23:10:21 +0200

    Add NodeJS code example
    Resolves: OBNB-465

---
+++ nodejs/asv4.js
@@ -0,0 +1,39 @@
+const crypto = require('crypto');
+
+function sign(key, data, encoding) {
+  return crypto.createHmac('sha256', key).update(data, 'utf8').digest(encoding);
+}
+
+function hashHex(string) {
+  return crypto.createHash('sha256').update(string, 'utf8').digest('hex');
+}
+
+function getSignatureKey(key, dateStamp, serviceName, regionName) {
+  const date = sign((`AWS4${key}`), dateStamp);
+  const region = sign(date, regionName);
+  const service = sign(region, serviceName);
+  return sign(service, 'aws4_request');
+}
+
+exports.sign = (options, clientId, clientSecret) => {
+  const algorithm = 'AWS4-HMAC-SHA256';
+  const amzDate = options.headers['x-amz-date'];
+  const dateStamp = amzDate.substr(0, 8);
+  const queryStringMatch = options.path.match(/(.*)\?/);
+  let canonicalUri = options.path;
+  if (queryStringMatch !== null) {
+    const firstMatchIndex = 1;
+    canonicalUri = queryStringMatch[firstMatchIndex];
+  }
+  const canonicalQuerystring = options.params;
+  const canonicalHeaders = `host:${options.host}\nx-amz-date:${amzDate}\n`;
+  const signedHeaders = 'host;x-amz-date';
+  const payloadHash = hashHex('');
+  const canonicalRequest = `GET\n${canonicalUri}\n${canonicalQuerystring}\n${canonicalHeaders}\n${signedHeaders}\n${payloadHash}`;
+  const credentialScope = `${dateStamp}/${options.region}/${options.service}/aws4_request`;
+  const stringToSign = `${algorithm}\n${amzDate}\n${credentialScope}\n${hashHex(canonicalRequest)}`;
+  const signingKey = getSignatureKey(clientSecret, dateStamp, options.service, options.region);
+  const signature = sign(signingKey, stringToSign, 'hex');
+  const credentialHeader = `Credential=${clientId}/${credentialScope}`;
+  return `${algorithm} ${credentialHeader}, SignedHeaders=${signedHeaders}, Signature=${signature}`;
+};
