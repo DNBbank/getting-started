58a0338c07462d33f2f385f1b6e5f12a615e295c
Rolf Erik Lekang <me@rolflekang.com>
2018-10-03 11:23:14 +0200

    Add env variable support for credentials in java

--- java/README.md
+++ java/README.md
@@ -1,3 +1,25 @@
-Replace `CLIENT_ID`, `CLIENT_SECRET` and `API_KEY` with your credentials.
+# Java example for DNB Open Banking APIs
 
-This code example uses parts of AWS' Java SDK.
+## Usage
+
+See [the main readme][] of the repo for a description on how to
+retrieve client id, client secret and api key.
+
+To configure the credentials use environment variables. You can
+put them in a file called `.env` in this directory with the
+following variables set.
+
+```
+CLIENT_ID=
+CLIENT_SECRET=
+API_KEY=
+```
+
+After adding the credentials you can run the example with
+
+```
+mvn exec:java -Dexec.mainClass="com.dnb.openbanking.gettingstarted.GettingStarted"
+```
+
+
+[the main readme]: (../README.md)
---
+++ java/src/main/java/com/dnb/openbanking/gettingstarted/Config.java
@@ -0,0 +1,42 @@
+package com.dnb.openbanking.gettingstarted;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+class Config {
+  private final static HashMap<String, String> config = new HashMap<>();
+
+  static {
+    config.putAll(loadEnvFile(".env"));
+    config.putAll(loadEnvFile("../.env"));
+  }
+
+  static String get(String key) {
+    return config.get(key);
+  }
+
+  private static Map<String, String> loadEnvFile(String path) {
+    Map<String, String> variables = new HashMap<>();
+    FileInputStream input = null;
+    try {
+      input = new FileInputStream(path);
+      Properties properties = new Properties();
+      properties.load(input);
+      for (final String name : properties.stringPropertyNames()) {
+        variables.put(name, properties.getProperty(name));
+      }
+    } catch (IOException ignored) {
+    } finally {
+      if (input != null) {
+        try {
+          input.close();
+        } catch (IOException ignored) {
+        }
+      }
+    }
+    return variables;
+  }
+}
