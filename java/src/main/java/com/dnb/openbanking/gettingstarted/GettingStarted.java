bd2bc1876eb5dd79f5d61377905b2bcea20e35e3
Didier Parat <didier.parat@gmail.com>
2018-12-13 12:37:32 +0100

    Add POST request example for initiating payment

--- java/src/main/java/com/dnb/openbanking/gettingstarted/GettingStarted.java
+++ java/src/main/java/com/dnb/openbanking/gettingstarted/GettingStarted.java
@@ -3,13 +3,13 @@ package com.dnb.openbanking.gettingstarted;
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.DefaultRequest;
 import com.amazonaws.Request;
-import com.amazonaws.Response;
 import com.amazonaws.auth.AWS4Signer;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.http.AmazonHttpClient;
 import com.amazonaws.http.ExecutionContext;
 import com.amazonaws.http.HttpMethodName;
+import java.io.InputStream;
 import java.net.URI;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -20,30 +20,40 @@ public class GettingStarted {
   private static final String AWS_REGION = "eu-west-1";
   private static final String AWS_SERVICE = "execute-api";
 
+  // Open Banking constants
   private static final String OPENBANKING_ENDPOINT = "https://developer-api-sandbox.dnb.no";
+  private static final String API_KEY_HEADER = "x-api-key";
+  private static final String JWT_TOKEN_HEADER = "x-dnbapi-jwt";
+
+  private static Request createRequest(final HttpMethodName httpMethodName, final String path) {
+    final Request request = new DefaultRequest(AWS_SERVICE);
+    request.setHttpMethod(httpMethodName);
+    request.setEndpoint(URI.create(OPENBANKING_ENDPOINT));
+    request.setResourcePath(path);
+    request.addHeader("Accept", "application/json");
+    request.addHeader("Content-type", "application/json");
+    request.addHeader(API_KEY_HEADER, Config.get("API_KEY"));
+    return request;
+  }
+
+  private static JSONObject signAndSendRequest(
+          final AWS4Signer signer, final AWSCredentials awsCredentials, final Request request) {
+    signer.sign(request, awsCredentials);
+    return new AmazonHttpClient(new ClientConfiguration())
+            .requestExecutionBuilder()
+            .executionContext(new ExecutionContext(true))
+            .request(request)
+            .errorResponseHandler(new ErrorResponseHandler(false))
+            .execute(new ResponseHandler(false)).getAwsResponse();
+  }
 
   private static String getApiToken(final AWS4Signer signer, final AWSCredentials awsCredentials) {
-    final Request apiTokenRequest = new DefaultRequest(AWS_SERVICE);
-    apiTokenRequest.setHttpMethod(HttpMethodName.GET);
-    apiTokenRequest.setEndpoint(URI.create(OPENBANKING_ENDPOINT));
-    apiTokenRequest.setResourcePath("/token");
-    apiTokenRequest.addHeader("Accept", "application/json");
-    apiTokenRequest.addHeader("Content-type", "application/json");
-    apiTokenRequest.addHeader("x-api-key", Config.get("API_KEY"));
+    final Request apiTokenRequest = createRequest(HttpMethodName.GET, "/token");
     apiTokenRequest.withParameter(
         "customerId", "{\"type\":\"SSN\", \"value\":\"29105573083\"}");
 
-    signer.sign(apiTokenRequest, awsCredentials);
-
-    final Response<JSONObject> apiTokenResponse
-        = new AmazonHttpClient(new ClientConfiguration())
-        .requestExecutionBuilder()
-        .executionContext(new ExecutionContext(true))
-        .request(apiTokenRequest)
-        .errorResponseHandler(new ErrorResponseHandler(false))
-        .execute(new ResponseHandler(false));
-    final JSONArray tokenInfoArray
-        = (JSONArray) (apiTokenResponse.getAwsResponse().get("tokenInfo"));
+    final JSONObject apiTokenResponse = signAndSendRequest(signer, awsCredentials, apiTokenRequest);
+    final JSONArray tokenInfoArray = (JSONArray) (apiTokenResponse.get("tokenInfo"));
     final JSONObject tokenInfo = (JSONObject) tokenInfoArray.get(0);
     final String jwtToken = (String) tokenInfo.get("jwtToken");
     System.out.println("JWT token: " + jwtToken);
@@ -52,35 +62,36 @@ public class GettingStarted {
 
   private static void getCustomerInfo(
       final String jwtToken, final AWS4Signer signer, final AWSCredentials awsCredentials) {
-     final Request customerRequest = new DefaultRequest(AWS_SERVICE);
-    customerRequest.setHttpMethod(HttpMethodName.GET);
-    customerRequest.setEndpoint(URI.create(OPENBANKING_ENDPOINT));
-    customerRequest.setResourcePath("/customers/current");
-    customerRequest.addHeader("Accept", "application/json");
-    customerRequest.addHeader("Content-type", "application/json");
-    customerRequest.addHeader("x-api-key", Config.get("API_KEY"));
-    customerRequest.addHeader("x-dnbapi-jwt", jwtToken);
+     final Request customerRequest = createRequest(HttpMethodName.GET, "/customers/current");
+    customerRequest.addHeader(JWT_TOKEN_HEADER, jwtToken);
 
-    signer.sign(customerRequest, awsCredentials);
+    final JSONObject customerResponse = signAndSendRequest(signer, awsCredentials, customerRequest);
+    System.out.println("Customer info: " + customerResponse.toString(4));
+  }
 
-    final Response<JSONObject> customerResponse
-        = new AmazonHttpClient(new ClientConfiguration())
-        .requestExecutionBuilder()
-        .executionContext(new ExecutionContext(true))
-        .request(customerRequest)
-        .errorResponseHandler(new ErrorResponseHandler(false))
-        .execute(new ResponseHandler(false));
-    System.out.println(
-        "Customer info: " + customerResponse.getAwsResponse().toString(4));
+  private static void postInitiatePayment(
+          final String jwtToken, final AWS4Signer signer, final AWSCredentials awsCredentials) {
+    final Request initiatePaymentRequest = createRequest(HttpMethodName.POST, "/payments");
+    initiatePaymentRequest.addHeader(JWT_TOKEN_HEADER, jwtToken);
+    final ClassLoader classLoader = GettingStarted.class.getClassLoader();
+    final InputStream initiatePaymentRequestBody
+        = classLoader.getResourceAsStream("InitiatePaymentRequestBody.json");
+    initiatePaymentRequest.setContent(initiatePaymentRequestBody);
+
+    final JSONObject initiatePaymentResponse
+        = signAndSendRequest(signer, awsCredentials, initiatePaymentRequest);
+    System.out.println("Initiate Payment response: " + initiatePaymentResponse);
   }
 
   public static void main(final String[] args) {
-    final AWSCredentials awsCredentials = new BasicAWSCredentials(Config.get("CLIENT_ID"), Config.get("CLIENT_SECRET"));
+    final AWSCredentials awsCredentials
+        = new BasicAWSCredentials(Config.get("CLIENT_ID"), Config.get("CLIENT_SECRET"));
     final AWS4Signer signer = new AWS4Signer();
     signer.setRegionName(AWS_REGION);
     signer.setServiceName(AWS_SERVICE);
 
     final String jwtToken = getApiToken(signer, awsCredentials);
     getCustomerInfo(jwtToken, signer, awsCredentials);
+    postInitiatePayment(jwtToken, signer, awsCredentials);
   }
 }
