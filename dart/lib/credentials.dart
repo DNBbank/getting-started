0c52ac9e25d3aed11433dba0e3229b7d84807e5f
Arne Molland <arneolaim@gmail.com>
2019-05-01 18:12:33 +0200

    🔑 Get .env from root folder

--- dart/lib/credentials.dart
+++ dart/lib/credentials.dart
@@ -2,8 +2,8 @@ import 'package:dotenv/dotenv.dart';
 import 'dart:io';
 
 Map<String, String> loadCredentials() {
-  if (File('.env').existsSync()) {
-    load('.env');
+  if (File('../.env').existsSync()) {
+    load('../.env');
   }
   assert(isEveryDefined(['CLIENT_ID', 'CLIENT_SECRET', 'API_KEY']));
   return env;
